f3a95d06dc9f6c3e74ecbda1ea7813c31025880a
Mikhail Sotnikov <thehofch@gmail.com>
2017-06-28 20:17:57 +0300

    init

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TelegramController extends Controller
+{
+    public function test()
+    {
+        $this->dispatch(new )
+
+        return 'Hello World';
+    }
+}
